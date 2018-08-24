8b1613a813f061d3be52374e20782f02f5f94fc6
Антон Тамбовцев (Ramfan) <grafvector725@gmail.com>
2018-08-24 17:12:04 +0300

    testing

--- src/componets/Forms/Auth.js
+++ src/componets/Forms/Auth.js
@@ -2,40 +2,25 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types';
 import {auth} from "../../Action-Creators/AC";
+import {Field, reduxForm} from 'redux-form'
 
 class Auth extends Component {
-
-    state = {
-        login: '',
-        password: ''
-    };
     render() {
+        const {handleSubmit} = this.props
         return (
             <div>
-               <input type="text" value={this.state.login} placeholder={'Login'} onChange={this.handleChangeLogin}/> <br/>
-               <input type="password" value={this.state.password} placeholder={'Password'} onChange={this.handleChangePassword}/> <br/>
-                <button onClick={this.handleSubmit}>Sign in</button>
+                <form onSubmit={handleSubmit}>
+                    <Field name='name' component='input' type='text'/>
+                    <Field name='surname' component='input' type='text'/>
+                    <button type='submit'>send</button>
+                </form>
             </div>
-        );
+        )
     }
-     handleChangeLogin = ev => {
-         this.setState({
-             login: ev.target.value
-         });
 
-    };
-    handleChangePassword = ev => {
-        this.setState({
-            password: ev.target.value
-        });
-
-    };
-    handleSubmit = () => {
-        const {auth} =this.props;
-        auth(this.state)
-    }
 }
 
-export default connect (state => ({
-    user: state.formReducer
-}), {auth})(Auth);
+export default reduxForm({
+    form: 'simpleForm',
+    onSubmit: values => console.log('sended', values)
+})(Auth);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux'
 import formReducer from "./formReducer";
+import {reducer} from 'redux-form'
 
 export default combineReducers({
-    formReducer
+    formReducer,
+    form: reducer
 })
