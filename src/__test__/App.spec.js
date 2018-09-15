c69b946023b16be6f927a1f0b9b8fed074bc4479
Антон Тамбовцев (Ramfan) <grafvector725@gmail.com>
2018-09-15 15:47:46 +0300

    not work

--- src/__test__/App.spec.js
+++ src/__test__/App.spec.js
@@ -2,19 +2,22 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import App from '../componets/App'
 import {Background, ButtonStyle, SpanColor} from "../componets/RooStyle";
-import Error from '../componets/Forms/Error'
+import {renderEditField} from '../componets/Forms/renderField'
+import Search from "../componets/Forms/Search";
 
-// describe('App component tests', () => {
-//     it('component will be render', function () {
-//         const app = shallow(<App />)
-//         expect(app.find('Link')).toHaveLength(2);
-//     });
-// });
+describe('App component tests', () => {
+    it('component will be render', function () {
+        const app = shallow(<App />)
+        expect(app.find('Link')).toHaveLength(2);
+    });
+});
 
 describe('Custom input component tests', () => {
+
     it('component will be render', function () {
-        const app = shallow(<Error/>)
-        expect(app.find('div')).toHaveLength(0);
+        const app = shallow(<Search />)
+        console.log(app.debug)
+        expect(app.find('div')).toHaveLength(1);
 
     });
 });
