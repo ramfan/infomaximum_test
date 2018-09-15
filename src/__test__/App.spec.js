19a5d30ee9e9fc20c1c0bf87c64a29ed6af68607
Антон Тамбовцев (Ramfan) <grafvector725@gmail.com>
2018-09-15 03:39:54 +0300

    add init test

--- src/__test__/App.spec.js
+++ src/__test__/App.spec.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import App from './App'
+import App from '../componets/App'
+import {Background, ButtonStyle, SpanColor} from "../componets/RooStyle";
 
 
 describe('function tests', () => {
     it('component will be render', function () {
         const app = shallow(<App />)
-        expect(app.find('div')).toHaveLength(2);
+        expect(app.find('Link')).toHaveLength(2);
     });
 });
